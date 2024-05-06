9825933365f63d131061da164661578188aa1c55
itzsamr <samarathiban@gmail.com>
2024-05-06 15:00:50 +0530

    commit - loops

---
+++ Python/Coding/day09_loops.py
@@ -0,0 +1,15 @@
+# for i in range(0,11):
+#     print(i)
+
+
+# While loop
+# count = 1
+# n = int(input())
+# while count < n + 1:
+#     print("✨" * count)
+#     count += 1
+
+# For loop
+n = int(input())
+for i in range(n + 1):
+    print("✨" * i)
