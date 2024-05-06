25a1ef45bee4396d84c49c5992ff19d92d6cd93c
itzsamr <samarathiban@gmail.com>
2024-05-06 11:56:24 +0530

    commit - list

--- Python/Coding/day09_list.py
+++ Python/Coding/day09_list.py
@@ -1,2 +1,11 @@
 marks = [98,75,40,80,90,45,80,60]
-print(len(marks))
+# print(len(marks))
+# print(marks[:4])
+# print(marks[::-1])
+marks.append(78)
+print(marks)
+
+# Add 65 after 40
+sci = 65
+marks.insert(3, sci)
+print(marks)
