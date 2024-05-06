5456991000ab63ed697037a30cf7198487970879
itzsamr <samarathiban@gmail.com>
2024-05-06 12:28:34 +0530

    commit - list

--- Python/Coding/day09_list.py
+++ Python/Coding/day09_list.py
@@ -1,11 +1,21 @@
-marks = [98,75,40,80,90,45,80,60]
+# marks = [98,75,40,80,90,45,80,60]
 # print(len(marks))
 # print(marks[:4])
 # print(marks[::-1])
-marks.append(78)
-print(marks)
+# marks.append(78)
+# print(marks)
 
 # Add 65 after 40
-sci = 65
-marks.insert(3, sci)
-print(marks)
+# sci = 65
+# marks.insert(3, sci)
+# print(marks)
+
+price_list = [1000, 1500, 400]
+price_list_copy = price_list ## Copy by reference
+price_list1 = [1000, 1500, 400]
+ 
+price_list_copy.append(600)
+price_list.append(700)
+price_list1.append(800)
+ 
+print(price_list, price_list_copy, price_list1)
