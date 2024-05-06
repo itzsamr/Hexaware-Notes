e9d040558ad45adb09c47f473e7a3c1b38a6d112
itzsamr <samarathiban@gmail.com>
2024-05-06 16:19:58 +0530

    commit - loops

--- Python/Coding/day09_loops.py
+++ Python/Coding/day09_loops.py
@@ -9,7 +9,51 @@
 #     print("✨" * count)
 #     count += 1
 
+
 # For loop
-n = int(input())
-for i in range(n + 1):
-    print("✨" * i)
+# n = int(input())
+# for i in range(n + 1):
+#     print("✨" * i)
+
+
+# p = [10, 20, 30]
+# for i in range(0, len(p)):
+#     p[i] = p[i] * 2
+# print(p)
+
+
+avengers = [
+    "Hulk",
+    "Iron man",
+    "Black widow",
+    "Captain america",
+    "Spider man",
+    "Thor",
+]
+
+
+# Output
+# [4, 8, 11, 15, 10, 4]
+
+
+# Task 4.1 - for loop
+# lengths = []
+# for avenger in avengers:
+#     lengths.append(len(avenger))
+# print(lengths)
+
+
+# Task 4.2 - List comprehension - (Use this)
+# lengths = [len(avenger) for avenger in avengers]
+# print(lengths)
+
+# Task 5.1
+lengths = []
+for avenger in avengers:
+    if len(avenger) > 10:
+        lengths.append(avenger)  # Append the avenger's name itself
+print(lengths)
+
+# Task 5.2
+lengths = [avenger for avenger in avengers if len(avenger) > 10]
+print(lengths)
