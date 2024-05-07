368641cf2b96354dd45e041b95625aaa48510364
itzsamr <samarathiban@gmail.com>
2024-05-07 09:56:04 +0530

    commit - set

--- Python/Coding/day09_sets.py
+++ Python/Coding/day09_sets.py
@@ -1,2 +1,10 @@
 colors = ["red", "blue", "red", "green", "pink", "blue"]
 print(set(colors))
+
+# Sets
+# 1. Mutable
+# 2. Unique
+# 3. Order is not guaranteed (no index)
+
+# add -> single item
+# update -> multiple item
