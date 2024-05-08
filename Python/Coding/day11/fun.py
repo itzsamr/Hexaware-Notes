9378e80d1c2e85886b8e5a6f6a1e47b2f22817e6
itzsamr <samarathiban@gmail.com>
2024-05-08 17:41:47 +0530

    day11 - exceptions

---
+++ Python/Coding/day11/fun.py
@@ -0,0 +1,17 @@
+# Type 1
+import cool
+
+print(cool.to_upper_case("Samar"))  # Hold ctrl + Click
+
+# Type 2
+from cool import to_upper_case
+
+print(to_upper_case("samarathiban"))
+
+# Type 3
+import cool as cl
+
+# Type 4 Inside folder
+from sample_.cool import to_upper_case
+
+print(to_upper_case("inside folder"))
---
+++ Python/Coding/sample_/cool.py
@@ -0,0 +1,11 @@
+def to_upper_case(text):
+    return text.upper()
+
+
+def main():
+    print(__name__)
+    print(to_upper_case("Sanjana"))
+
+
+if __name__ == "__main__":
+    main()
