82b15f69c3da49bda28e9a21fe087a1965860992
itzsamr <samarathiban@gmail.com>
2024-05-09 11:03:10 +0530

    day11 - exceptions

--- Python/Coding/day11/day11_exceptions.py
+++ Python/Coding/day11/day11_exceptions.py
@@ -1,80 +1,121 @@
-# # Compile-time error (Syntax error)
-# # def math_divide(n1, n2):
-# #     try:
-# #         result = n1 / n2
-# #         return result
+from datetime import datetime
 
-# #     # Generic message
-# #     except:
-# #         return "Oops. 🤭 An Error happened"
-
-
-# # Errors
-# # 1. try
-# # 2. except
-# # 3. else
-# # 4. finally
-# def divide_eggs(n1, n2):
+# Compile-time error (Syntax error)
+# def math_divide(n1, n2):
 #     try:
-#         if n1 < 0:
-#             raise ValueError("Eggs cannot be negative 🤭")
-#         if n2 < 0:
-#             raise ValueError("People cannot be negative 🤭")
-
-#         # Code is shield 🛡️
 #         result = n1 / n2
+#         return result
 
-#     # Specific message
-#     except ZeroDivisionError:
-#         return "You cannot divide by zero! 💀"
-#     except ValueError as e:
-#         return f"Invalid number: {e}"
-#     else:
-#         # When no error
-#         print("Division was successful ✅")
-#     finally:
-#         # No matter
-#         print("Operation done 😊✌️")
-
-#     return result
+#     # Generic message
+#     except:
+#         return "Oops. 🤭 An Error happened"
 
 
-# # Runtime error
-# print(divide_eggs(10, -5))
-# print(divide_eggs(-10, 5))
-# print(divide_eggs(10, 5))
-# print(divide_eggs(10, 0))  # Execution stops
-# print(divide_eggs(15, 3))
+# Errors
+# 1. try
+# 2. except
+# 3. else
+# 4. finally
+def divide_eggs(n1, n2):
+    try:
+        # Business logic error
+        if n1 < 0:
+            # raise - throwing error
+            raise ValueError("Eggs cannot be negative 🤭")
+        if n2 < 0:
+            raise ValueError("People cannot be negative 🤭")
 
-# # Handle error
+        # Code is shield 🛡️
+        result = n1 / n2
 
-# from datetime import datetime
+    # Specific message
+    except ZeroDivisionError:
+        return "You cannot divide by zero! 💀"
+    except ValueError as e:
+        return f"Invalid number: {e}"
+    else:
+        # When no error
+        print("Division was successful ✅")
+    finally:
+        # No matter
+        print("Operation done 😊✌️")
 
-# print(datetime.now().weekday())
-# print(datetime.now().day)
+    return result
+
+
+# Task 1: Handling string in birth_year
 
 
 # Calculate age & Handle errors
-from datetime import datetime
+# def calculate_age():
+#     try:
+#         current_year = datetime.now().year
+#         birth_year = input("Please provide your birth year: ")
+
+#         age = current_year - int(birth_year)
+#         print(f"Your age is {age}")
+#     except ValueError as err:
+#         print("Invalid number: ", err)
+
+
+# Task 2:  -ve value, future years (Logical Errors)
 
 
 def calculate_age():
-    while True:
-        try:
-            birth_year = int(input("Please provide your birth year: "))
-            if birth_year > datetime.now().year:
-                print("You can't be born in the future!")
-                continue
-            elif birth_year < 0:
-                print("Please enter a valid birth year.")
-                continue
-            else:
-                current_year = datetime.now().year
-                age = current_year - birth_year
-                print(f"Your age is {age}")
-                break
-        except ValueError:
-            print("Please enter a valid birth year as a number.")
+    try:
+        current_year = datetime.now().year
+        birth_year = int(input("Please provide your birth year: "))  # Runtime error
+
+        if birth_year < 0:
+            # Handling logical error
+            raise ValueError("Year cannot be negative")
+        if birth_year > current_year:
+            raise ValueError("Your not flash to be from the future ⚡")
+
+        age = current_year - birth_year
+        print(f"Your age is {age}")
+    except ValueError as err:
+        print("Invalid number: ", err)
+    except Exception as err:
+        print("This catch all!", err)
 
 
-calculate_age()
+def main():
+    # Runtime error
+    print(divide_eggs(10, -5))
+    print(divide_eggs(-10, 5))
+    print(divide_eggs(10, 5))
+    print(divide_eggs(10, 0))  # Execution stops
+    print(divide_eggs(15, 3))
+    print(datetime.now().weekday())
+    print(datetime.now().day)
+
+
+# Every has same base class -> Exception
+class NegativeNumberError(Exception):
+    def __init__(self, value):
+        self.value = value
+        self.message = "Negative numbers are not allowed"
+        super().__init__(self.message)
+
+    # Overriding Base class __str__
+    def __str__(self):
+        return f"{self.value} -> {self.message}"
+
+
+# Create you own Error Class
+
+
+def only_positive_num():
+    try:
+        x = -10
+        if x < 0:
+            raise NegativeNumberError(x)
+    except NegativeNumberError as err:
+        print(err)
+
+
+if __name__ == "__main__":
+    # main()
+    # calculate_age()
+    only_positive_num()
